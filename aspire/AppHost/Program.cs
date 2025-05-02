bcde888a3fc4c794c65a817e74113471ac66649f
hangy <hangy@hangy.de>
2025-05-02 22:27:50 +0200

    refactor: Add EventStore reference to Program.cs and update package references in AppHost.csproj

--- aspire/AppHost/Program.cs
+++ aspire/AppHost/Program.cs
@@ -2,7 +2,11 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var eventStore = builder.AddEventStore("eventstore")
+                        .WithOtlpExporter();
+
 var api = builder.AddProject<CasinoRoyale_Api>("api")
+                 .WithReference(eventStore)
                  .WithExternalHttpEndpoints();
 
 builder.AddNpmApp("pwa", workingDirectory: "../../", scriptName: "dev")
